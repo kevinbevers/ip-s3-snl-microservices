4160b3041d31ce693dc0c250481bcf1e9429deab
Kevin Bevers <kevin.bevers@hotmail.com>
2020-10-16 18:43:10 +0200

    test update

--- smitenoobleague-web-app/pages/api/__tests__/hello.test.js
+++ smitenoobleague-web-app/pages/api/__tests__/hello.test.js
@@ -2,4 +2,5 @@ const { interopDefault } = require("next/dist/next-server/server/load-components
 
 describe("Hello", () => {
     it("Should return a status code of 200", () => {});
+    it("testing if only on master", () => {});
 });
