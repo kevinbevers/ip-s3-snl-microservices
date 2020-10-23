ff07e479d7191f7188c20fe1e8e797e1b3333116
Kevin Bevers <kevin.bevers@hotmail.com>
2020-10-23 20:08:06 +0200

    TeamSelectpage created and styled

--- smitenoobleague-web-app/pages/stats/team.js
+++ smitenoobleague-web-app/pages/stats/team.js
@@ -1,12 +1,51 @@
 //default page stuff
 import NavBar from 'src/components/NavBar';
 import Footer from 'src/components/Footer';
+//bootstrap components
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { Container, Card, Image, FormControl, InputGroup } from 'react-bootstrap';
+import TeamCard from 'src/components/TeamCard';
+
 
 export default function team() {
-    return (
-      <>      
-        <NavBar />
-        <Footer />
-      </>
-    );
+  return (
+    <>
+      <NavBar />
+      <Container className="mt-4">
+        <Row>
+          <Col></Col>
+          <Col md={4} className="d-flex justify-content-center">
+            <FormControl
+              className="searchinput"
+              placeholder="Search team..."
+              aria-label="Search teams"
+              aria-describedby="Search"
+            />
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row className="mt-4">
+          <Col md={6 } className="mx-auto">
+            {/*Team cards */}
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
 }
