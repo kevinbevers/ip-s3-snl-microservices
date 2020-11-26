14dd086a2e2122b1e14c39ca358e1cd8a5ae9329
Kevin Bevers <kevin.bevers@hotmail.com>
2020-11-26 15:28:23 +0100

    test if warning pop's up

--- smitenoobleague-web-app/__tests__/captainpage.test.js
+++ smitenoobleague-web-app/__tests__/captainpage.test.js
@@ -8,7 +8,14 @@ import 'jest-canvas-mock';
 import Captain from '../pages/captainpage';
 
 describe("Test userinput on the captainpage", () => {
-    it("Pressing submit without filling in a gameID should return a message / warning to the user", () => {
-       expect(true);
+    it("Pressing submit without filling in a gameID should return a message / warning to the user", async () => {
+       render(<Captain />);
+       await waitFor(() => { //user waits for the form to have rendered
+            expect(screen.findByTestId("formSubmitGameIdCaptainPage")).toBeInTheDocument();
+       });
+       //user clicks on the submit button without entering data in the inputfield
+       fireEvent.click(screen.findByTestId("buttonSubmitGameIdCaptainPage"));
+       //user expects error message to show
+       expect((await screen.findByTestId("formSubmitMessageCaptainPage")));
     });
 });
