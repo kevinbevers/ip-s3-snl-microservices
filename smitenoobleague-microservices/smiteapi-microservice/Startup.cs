608367243d24ed929d4d6a58b4622f789f2cc2de
Kevin Bevers <kevin.bevers@hotmail.com>
2020-12-06 15:52:39 +0100

    test splitted copy

--- smitenoobleague-microservices/smiteapi-microservice/Startup.cs
+++ smitenoobleague-microservices/smiteapi-microservice/Startup.cs
@@ -33,8 +33,8 @@ namespace smiteapi_microservice
 
         public IConfiguration Configuration { get; }
         string dbpass = Environment.GetEnvironmentVariable("DB_Password");
-        string DevId = Environment.GetEnvironmentVariable("DevId");
-        string AuthKey = Environment.GetEnvironmentVariable("AuthKey");
+        string DevId = Environment.GetEnvironmentVariable("Smite_Api_DevId");
+        string AuthKey = Environment.GetEnvironmentVariable("Smite_Api_AuthKey");
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
