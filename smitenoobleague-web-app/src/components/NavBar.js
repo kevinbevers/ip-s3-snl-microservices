b5809e84494a0c063123f33a4c0ac7577950ae53
Kevin Bevers <kevin.bevers@hotmail.com>
2021-01-09 18:49:29 +0100

    navbar text color change

--- smitenoobleague-web-app/src/components/NavBar.js
+++ smitenoobleague-web-app/src/components/NavBar.js
@@ -20,19 +20,19 @@ export default function NavBar({LoginSession}) {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                    {LoginSession?.isCaptain ? <><Nav.Link href="/captainpage" className={router?.pathname == "/captainpage" ? "active" : ""}>Captain page</Nav.Link></> : <> </>}
-                    <NavDropdown title="Stats" id="collapsible-nav-dropdown">
-                        <NavDropdown.Item href="/stats/team" className={router?.pathname.includes("/stats/team") ? "active" : ""}>Team stats</NavDropdown.Item>
+                    {LoginSession?.isCaptain ? <><Nav.Link href="/captainpage" className={router?.pathname == "/captainpage" ? "active darktext" : "darktext"}>Captain page</Nav.Link></> : <> </>}
+                    <NavDropdown title="Stats" id={router?.pathname.includes("/stats") ? "collapsible-nav-dropdown active" : "collapsible-nav-dropdown"}>
+                        <NavDropdown.Item href="/stats/team" className={router?.pathname.includes("/stats/team") ? "active darktext" : "darktext"}>Team stats</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="/stats/player" className={router?.pathname.includes("/stats/player")  ? "active" : ""}>Player stats</NavDropdown.Item>
+                        <NavDropdown.Item href="/stats/player" className={router?.pathname.includes("/stats/player")  ? "active darktext" : "darktext"}>Player stats</NavDropdown.Item>
                     </NavDropdown>
-                    <Nav.Link href="/schedules" className={router?.pathname == "/schedules" ? "active" : ""}>Schedules</Nav.Link>
-                    <Nav.Link href="/matchhistory" className={router?.pathname == "/matchhistory" ? "active" : ""}>Match history</Nav.Link>
-                    <Nav.Link href="/standings" className={router?.pathname == "/standings" ? "active" : ""}>Standings</Nav.Link>
-                    <Nav.Link href="/leaderboards" className={router?.pathname == "/leaderboards" ? "active" : ""}>Leaderboards</Nav.Link>
-                    <Nav.Link href="/news" className={router?.pathname == "/news" ? "active" : ""}>News</Nav.Link>
-                    <Nav.Link href="/rules" className={router?.pathname == "/rules" ? "active" : ""}>Rules</Nav.Link>
-                    <Nav.Link href="/faq" className={router?.pathname == "/faq" ? "active" : ""}>FAQ</Nav.Link>
+                    <Nav.Link href="/schedules" className={router?.pathname == "/schedules" ? "active darktext" : "darktext"}>Schedules</Nav.Link>
+                    <Nav.Link href="/matchhistory" className={router?.pathname == "/matchhistory" ? "active darktext" : "darktext"}>Match history</Nav.Link>
+                    <Nav.Link href="/standings" className={router?.pathname == "/standings" ? "active darktext" : "darktext"}>Standings</Nav.Link>
+                    <Nav.Link href="/leaderboards" className={router?.pathname == "/leaderboards" ? "active darktext" : "darktext"}>Leaderboards</Nav.Link>
+                    <Nav.Link href="/news" className={router?.pathname == "/news" ? "active darktext" : "darktext"}>News</Nav.Link>
+                    <Nav.Link href="/rules" className={router?.pathname == "/rules" ? "active darktext" : "darktext"}>Rules</Nav.Link>
+                    <Nav.Link href="/faq" className={router?.pathname == "/faq" ? "active darktext" : "darktext"}>FAQ</Nav.Link>
                     </Nav>
                     <Nav>
                         {LoginSession?.user != null ? 
