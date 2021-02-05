fcbe0007b9529148dfe670d158e1dab29232119d
Kevin Bevers <kevin.bevers@hotmail.com>
2021-02-05 11:23:33 +0100

    updated roadmap dates

--- smitenoobleague-web-app/pages/roadmap.js
+++ smitenoobleague-web-app/pages/roadmap.js
@@ -37,7 +37,7 @@ export default function roadmap({LoginSession}) {
     icon={<FaBinoculars />}
   >
     <h3 className="vertical-timeline-element-title">Free agent area</h3>
-    <h6 className="vertical-timeline-element-subtitle">Coming end of Q1 2021</h6>
+    <h6 className="vertical-timeline-element-subtitle">Coming Q2 2021</h6>
     <p>
       Add yourself into a list of free agents, with experiences, preffered roles, and contact information.<br />
       Captains of teams can look in here to find players to fill their teams with. Or maybe you can even find other people to form your own team.
@@ -49,7 +49,7 @@ export default function roadmap({LoginSession}) {
     icon={<FaUsers />}
   >
     <h3 className="vertical-timeline-element-title">Roster page per division</h3>
-    <h6 className="vertical-timeline-element-subtitle">Coming early Q2 2021</h6>
+    <h6 className="vertical-timeline-element-subtitle">Coming Q2 2021</h6>
     <p>
       Easily see which teams are competing in each division.
     </p>
@@ -72,7 +72,7 @@ export default function roadmap({LoginSession}) {
      icon={<MdShowChart />}
   >
     <h3 className="vertical-timeline-element-title">Player Championships</h3>
-    <h6 className="vertical-timeline-element-subtitle">Coming end of Q2 2021</h6>
+    <h6 className="vertical-timeline-element-subtitle">Coming Q3 2021</h6>
     <p>
       Player Championships offer a way to see what player is performing the best in a specific league. And give player the chance to compete on a personal level.
     </p>
@@ -85,7 +85,7 @@ export default function roadmap({LoginSession}) {
     icon={<BsTable />}
   >
     <h3 className="vertical-timeline-element-title">Other schedule types</h3>
-    <h6 className="vertical-timeline-element-subtitle">Coming early Q3 2021</h6>
+    <h6 className="vertical-timeline-element-subtitle">Coming in the future</h6>
     <p>
       Tournament brackets, Faster splits,
       Best of 1's, Best of 5's
